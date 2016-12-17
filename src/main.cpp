2fae67f2380abfa79622b45d3490479cf50c5e21
Edward Hobbs <eh362@kent.ac.uk>
2016-12-17 17:23:50 +0000

    Added Adafruit libraries, more fiddling with main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include "Arduino.h"
 
+const int X_POS = A0;
+const int Y_POS = A1;
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(5000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(5000);
+  int pos = analogRead(A1);
+  Serial.println(pos);
 }
