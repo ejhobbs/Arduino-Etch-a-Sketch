04dfe87d5441fabe6f67231eb438a577e2aa57ce
Edward Hobbs <eh362@kent.ac.uk>
2016-12-17 19:14:45 +0000

    i think my oled is broken gonna have to sit like this until the new SPI one comes through

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,25 @@
 #include "Arduino.h"
 #include "Wire.h"
-#include "SPI.h"
-#include "Adafruit_GFX.h"
-#include "Adafruit_SSD1306.h"
+#include "U8g2lib.h"
 
-const int X_POS = A0;
-const int Y_POS = A1;
+
+#define X_POS A0
+#define Y_POS A1
+#define CLCK 0
+#define DATA 0
+#define CS 0
+#define DC 0
+
+U8G2_SH1106_128X64_NOANME_1_4W_HW_SPI display(U8G2_R0, CLCK, DATA, CS, DC);
+
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
+  display.begin();
+  pinMode(X_POS, INPUT);
+  pinMode(Y_POS, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  int pos = analogRead(A1);
-  Serial.println(pos);
+  //TODO
 }
