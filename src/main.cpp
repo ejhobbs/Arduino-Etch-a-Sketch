e3d524b77e268f3e15e019f0266de317c9b6201f
Edward Hobbs <eh362@kent.ac.uk>
2017-01-12 15:45:56 +0000

    cleaned up code, renamed stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,3 @@
-/*
- * Demo for SSD1306 based 128x64 OLED module using u8glib.
- * See https://github.com/pacodelgado/arduino/wiki/SSD1306-based-OLED-connected-to-Arduino
- * for more information.
- *
- *  ---------------------------------------------------------------------
- * U8glib library copyright notice:
- * Universal 8bit Graphics Library, http://code.google.com/p/u8glib/
- * Copyright (c) 2012, olikraus@gmail.com
- * All rights reserved.
- */
 #include <Arduino.h>
 #include "U8g2lib.h"
 
@@ -20,9 +9,8 @@
  *   DC=A0=Pin 9
  *   Reset=Pin 10
  */
+U8GLIB_SH1106_128X64_NONAME_1_4W_SW_SPI u8g2(U8G2_R0, 12, 11, 8, 9, 10);
 
-
-U8GLIB_SH1106_128X64_NONAME_1_4W_SW_SPI u8g(U8G2_R0, 12, 11, 8, 9, 10);
 const int POT_LEFT = A5;
 const int POT_RIGHT = A4;
 
@@ -31,21 +19,21 @@ int map_input(int, int, int);
 
 void setup()
 {
-  u8g.begin();
+  u8g2.begin();
 }
 
 void loop()
 {
-  u8g.firstPage();
+  u8g2.firstPage();
   do {
     draw();
-  } while(u8g.nextPage());
+  } while(u8g2.nextPage());
 }
 
 void draw(void) {
   int x_pos = map_input(POT_LEFT, 0, 127);
   int y_pos = map_input(POT_RIGHT, 0, 63);
-  u8g.drawPixel(x_pos,y_pos);
+  u8g2.drawPixel(x_pos,y_pos);
 }
 
 int map_input(int POT, int min, int max){
