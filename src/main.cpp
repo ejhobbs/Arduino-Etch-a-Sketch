e4ae408e927588345a9275d81a6c8358d6bb2090
Edward Hobbs <eh362@kent.ac.uk>
2017-01-12 15:34:57 +0000

    updated constructor to new style

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@
  */
 
 
-U8GLIB_SSD1306_128X64 u8g(12, 11, 8, 9, 10);
+U8GLIB_SH1106_128X64_NONAME_1_4W_SW_SPI u8g(U8G2_R0, 12, 11, 8, 9, 10);
 const int POT_LEFT = A5;
 const int POT_RIGHT = A4;
 
